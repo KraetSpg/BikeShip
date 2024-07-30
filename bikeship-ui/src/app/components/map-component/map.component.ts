a4f7d36c9cb83b5b2b4598df92f2920e264b67cf
kraet <156175162+KraetSpg@users.noreply.github.com>
2024-07-30 14:31:00 +0000

    edited map

--- bikeship-ui/src/app/components/map-component/map.component.ts
+++ bikeship-ui/src/app/components/map-component/map.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import Map from 'ol/Map.js';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
+import { MapService } from '../../services/map/map.service';
 
 @Component({
   selector: 'app-map-component',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrl: './map.component.css',
+  providers: [MapService]
 })
 export class MapKomponent implements OnInit {
-  title = 'bikeship-ui';
+  private mapService = inject(MapService)
 
   ngOnInit(): void {
 
---
+++ bikeship-ui/src/app/services/map/map.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { ListenOptions } from "net";
+
+@Injectable({  providedIn: 'root'})
+export class MapService {
+
+    getBikerMeetUps(){
+
+    }
+
+}
